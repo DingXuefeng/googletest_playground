74dd8e52ac0af9a766c91c8e40e5348b58e7007e
Xuefeng, Ding <xuefeng.ding.physics@gmail.com>
2022-03-01 21:08:42 -0500

    add initial files

---
+++ main.cc
@@ -0,0 +1,10 @@
+/*****************************************************************************/
+// Author: Xuefeng Ding <xuefengd@princeton.edu>
+// Institute: Department of Physics, Princeton University
+// Date: 3/1/22
+// Version: v1.0
+// Description: GooStats, a statistical analysis toolkit that runs on GPU.
+//
+// All rights reserved. 2022 copyrighted.
+/*****************************************************************************/
+
---
+++ src/A.h
@@ -0,0 +1,21 @@
+/*****************************************************************************/
+// Author: Xuefeng Ding <xuefengd@princeton.edu>
+// Institute: Department of Physics, Princeton University
+// Date: 3/1/22
+// Version: v1.0
+// Description: GooStats, a statistical analysis toolkit that runs on GPU.
+//
+// All rights reserved. 2022 copyrighted.
+/*****************************************************************************/
+
+#ifndef GOOGLTEST_PLAYGROUND_A_H
+#define GOOGLTEST_PLAYGROUND_A_H
+
+namespace A {
+    class A {
+
+    };
+}
+
+
+#endif //GOOGLTEST_PLAYGROUND_A_H
